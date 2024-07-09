8f23cfb36e839021e1fa213cd8f7cac151f9a1ad
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-09 00:00:01 -0400

    feat: add startGame implementation

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -17,15 +17,48 @@ struct ContentView: View {
             List {
                 Section {
                     TextField("Enter your new word", text: $newWord)
+                        .textInputAutocapitalization(.never)
                 }
                 
                 Section {
                     ForEach(usedWords, id: \.self) { word in
-                        Text(word)
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
                     }
                 }
             }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else { return }
+        
+        // extra validation
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle")
     }
 }
 
